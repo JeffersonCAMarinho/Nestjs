8b001c31b16dc01c3f04aa4bebb0ecebef0f2ae5
Jefferson Marinho <jeffersonmarinho@bcard.net>
2021-06-25 14:02:24 -0300

    Teste

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CoffeesModule } from './coffees/coffees.module';
+
+
+@Module({
+  imports: [CoffeesModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
